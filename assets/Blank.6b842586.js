1327c78d310ff6c7582c845ebe0d185e8e02df45
Marcelo Silva <msr88957@gmail.com>
2022-05-25 23:28:09 -0300

    Updates

---
+++ assets/Blank.6b842586.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./PageWrapper.6420503f.js";import{j as t,m as e}from"./index.e62267ef.js";const _={name:"Blank",setup(r){return(o,c)=>(t(),e(a,{title:"Blank"}))}};export{_ as default};
---
+++ assets/PageFooter.0fc0467f.js
@@ -0,0 +1,1 @@
+import{j as e,k as o}from"./index.e62267ef.js";const t={class:"flex flex-col flex-shrink-0 w-full gap-2 px-6 py-4 lg:flex-row lg:justify-between"},a={name:"PageFooter",setup(l){return(r,s)=>(e(),o("footer",t))}};export{a as _};
